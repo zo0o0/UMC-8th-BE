5a7b74d4c7041df21ba0f35a6f0a403e950af6d8
김용민 <dydals3440@gmail.com>
2025-03-06 20:14:44 +0900

    feat: 비인증 이미지 업로드 기능 구현 및 스웨거 문서 추가

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -15,8 +15,8 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { ResponseMessage } from '../common/decorators/response-message-decorator';
+import { Public } from '../auth/decorators/public.decorator';
 
-@ApiBearerAuth()
 @ApiTags('uploads')
 @Controller('uploads')
 export class UploadsController {
@@ -25,8 +25,9 @@ export class UploadsController {
   @Post()
   @ResponseMessage('이미지 업로드 성공')
   @UseInterceptors(FileInterceptor('file'))
+  @ApiBearerAuth()
   @ApiOperation({
-    summary: '이미지 업로드',
+    summary: '이미지 업로드(인증)',
     description:
       '이미지 업로드 성공 시 URL을 반환 받습니다. 이미지 필드를 요구하는 곳에 해당 URL을 첨부하시면 됩니다. ex) Lp생성(POST /v1/lps)의 thumbnail 필드에 이미지 업로드 성공 후 반환되는 imageUrl을 첨부하시면 됩니다.',
   })
@@ -57,6 +58,45 @@ export class UploadsController {
     },
   })
   uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return { imageUrl: this.uploadsService.getImageUrl(file.filename) };
+  }
+
+  @Post('public')
+  @Public()
+  @ResponseMessage('이미지 업로드 성공')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiOperation({
+    summary: '이미지 업로드(비인증)',
+    description:
+      '이미지 업로드 성공 시 URL을 반환 받습니다. 이미지 필드를 요구하는 곳에 해당 URL을 첨부하시면 됩니다. ex) Lp생성(POST /v1/lps)의 thumbnail 필드에 이미지 업로드 성공 후 반환되는 imageUrl을 첨부하시면 됩니다.',
+  })
+  @ApiConsumes('multipart/form-data') // FormData 형식 사용
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary', // 파일 업로드 필드
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: '이미지 업로드 성공.',
+    schema: {
+      example: {
+        status: true,
+        message: '이미지 업로드 성공.',
+        statusCode: 201,
+        data: {
+          imageUrl: 'http://localhost:8000/uploads/1741127947806-552079898.png',
+        },
+      },
+    },
+  })
+  uploadImage(@UploadedFile() file: Express.Multer.File) {
     return { imageUrl: this.uploadsService.getImageUrl(file.filename) };
   }
 }
