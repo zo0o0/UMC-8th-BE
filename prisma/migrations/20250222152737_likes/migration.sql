7f3828b47d46fa484b3a6047a52eb8a5c3297e60
김용민 <dydals3440@gmail.com>
2025-03-16 23:49:37 +0900

    chore: prisma setup initialization

--- prisma/migrations/20250222152737_likes/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,lpId]` on the table `Like` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Like_userId_lpId_key" ON "Like"("userId", "lpId");
